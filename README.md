49125f04ca534c8bd53d27c85cf0094ffb0ab97e
SisterToch <ibekwetestimony3@gmail.com>
2023-09-13 09:58:24 -0700

    git push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a to-do list project, and i will be listing every feature it has. I hope you enjoy iy
+This is a to-do list project, and i will be listing every feature it has. I hope you enjoy iT.
+While perusing, you will realize that i didnt use append where should have, and instead used ".insert". This is because tkinter listbox widget is a part of the GUI, responsoble for displayin the tasks in an organized manner. It doesnt treat the tasks as one whole, but different items.
--- to-do.py
+++ to-do.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 import tkinter as tk
-import pickle """this module helps save and load tasks to and from a file"""
+import pickle
+"""this module helps save and load tasks to and from a file"""
 
-def add_task(): """appears everytime i click add task button"""
-    task = task_entry.get() """get the task from task_entry and save in task"""
-    if task: """checks if task is empty"""
-        tasks.append(task) """append to tasks"""
-        update_task_list() """to refresh the tasklist"""
-        task_entry.delete(0, tk.End) """delete all the text from 0 to end of text"""
+def add_task():
+    """appears everytime i click add task button"""
+    task = task_entry.get()
+    """get the task from task_entry and save in task"""
+    if task:
+        """checks if task is empty"""
+        tasks.append(task)
+        """append to tasks"""
+        update_task_list()
+        """to refresh the tasklist"""
+        task_entry.delete(0, tk.End)
+        """delete all the text from 0 to end of text"""
         save_tasks()
 
 def remove_task():
@@ -18,3 +25,43 @@ def remove_task():
         del tasks[index]
         update_task_list()
         save_tasks()
+
+def update_task_list():
+    task_listbox.delete(0, tk.END)
+    """this clears the existing tasks in the list box"""
+    for task in tasks:
+        task_listbox.insert(tk.End, task)
+        """inserts the new task into the taskslistbox at the end of the list"""
+
+
+def save_tasks():
+    with open('tasks.pkl', 'wb') as f:
+        pickle.dump(tasks, f)
+
+try:
+    with open('tasks.pkl', 'rb') as f:
+        tasks = pickle.load(f)
+except FileNotFoundError:
+    tasks = []
+
+"""create the application window"""
+app = tk.Tk()
+app.title("To-Do List")
+
+""" Create and configure widgets"""
+task_label = tk.Label(app, text="Enter task:")
+"""creates a label widget('tk.Label') with the text "enter task"""
+task_label.pack(pady=5)
+"""places the task_label in the GUI,with vertical padding 5pixels below the label"""
+task_entry = tk.Entry(app, width=40)
+task_entry.pack(pady=5)
+add_button = tk.Button(app, text="Add Task", command=add_task)
+add_button.pack()
+remove_button = tk.Button(app, text="Remove Task", command=remove_task)
+remove_button.pack()
+task_listbox = tk.Listbox(app, selectmode=tk.SINGLE, width=40, height=10)
+task_listbox.pack(pady=10)
+update_task_list()
+
+"""start the GUI loop"""
+app.mainloop()
